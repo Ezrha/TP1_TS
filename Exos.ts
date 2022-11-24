a21a8a9e500300404ee67ca720bce47a3ca432d6
Nathan Joudet <nathanjoudet@orange.fr>
2022-11-24 15:11:07 +0100

    Exercice3 Correction

--- Exos.ts
+++ Exos.ts
@@ -2,30 +2,28 @@
 // Créez une fonction qui prendra un prénom en paramètre. Votre fonction affichera :
 // "Bonjour " suivi du prénom dans un console.log.
 
-/**
- * Cette fonction retourne "Bonjour" + le prenom passé en paramètre
- * Type de paramètre => String
- * Type retour de la fonction => void car elle ne retourne rien
- */
- function appel(prenom:string) :void  {
-    console.log("Bonjour " + prenom);
-}
-appel("Nathan");
-
+// /**
+//  * Cette fonction retourne "Bonjour" + le prenom passé en paramètre
+//  * @param prenom
+//  * Type retour de la fonction => void car elle ne retourne rien
+//  */
+//  function appel(prenom:string) :void  {
+//     console.log("Bonjour " + prenom);
+// }
+// appel("Nathan");
 
 // Exercice 2
-// Créez une fonction qui prendra deux nombres en paramètre.
-// Votre fonction affichera la somme de ces deux nombres dans un console.log
-/**
- * Cette fonction retourne la somme de deux nombres en paramètre
- * @param number1 => number
- * @param number2 => number
- */
- function double(number1:number, number2:number) :void{
-    console.log(number1 + number2);
-}
-double(5,3);
-
+// // Créez une fonction qui prendra deux nombres en paramètre.
+// // Votre fonction affichera la somme de ces deux nombres dans un console.log
+// /**
+//  * Cette fonction retourne la somme de deux nombres en paramètre
+//  * @param number1 => premier nombre passé en paramètre
+//  * @param number2 => deuxieme nombre passé en paramètre
+//  */
+//  function double(number1:number, number2:number) :void{
+//     console.log(number1 + number2);
+// }
+// double(5,3);
 
 // Exercice 3
 // Créez une fonction qui prendra en paramètre deux nombres :
@@ -38,33 +36,42 @@ double(5,3);
  * @param prixVente => String
  * @returns => Chaine de caractère
  */
-function vente(coutFabrication:number, prixVente:number) :string {
-    let profit:number = prixVente - coutFabrication;
-    let perte:number = coutFabrication - prixVente;
-    if (prixVente <= coutFabrication) {
-       return `Perte de ${perte}`;
-    } else {
-        return `Gain de ${profit}`;
-    };
+function vente(coutFabrication: number, prixVente: number): string {
+  let profit: number = prixVente - coutFabrication;
+  let perte: number = coutFabrication - prixVente;
+  if (prixVente <= coutFabrication) {
+    return `Perte de ${perte}`;
+  } else {
+    return `Gain de ${profit}`;
+  }
 }
-console.log(vente(15,15));
+console.log(vente(15, 15));
 
-
-
-
-
-
-
-
-
-
-
-
+// Exercice 3 (CORRECTION)
+/**
+ * Calcul le bénéfice ou la perte d'une transaction en fonction de son cout et de son prix de vente
+ * @param coutFabrication prix de vente du produit
+ * @param prixVente prix d'achat du produit
+ * @returns phrase qui indique le bénéfice/perte
+ */
+function calculerBenefice(coutFabrication: number, prixVente: number): string {
+  const benefice: number = prixVente - coutFabrication;
+  if (benefice < 0) {
+    return `Perte de ${-benefice}`;
+  } else if (benefice > 0) {
+    return `Gain de ${+benefice}`;
+  } else {
+    return "Vive le bénévolat";
+  }
+}
+console.log(calculerBenefice(10, 20));
+console.log(calculerBenefice(20, 10));
+console.log(calculerBenefice(10, 10));
 
 // function exemple(param:type) :type {
 //     let nomVariable:type = ;
 //     return "";
 // }
 //
-// si "else" pas besoin de mettre le "return "" " 
-// si "else if" besoin de mettre le "return "" " 
+// si "else" pas besoin de mettre le "return "" "
+// si "else if" besoin de mettre le "return "" "

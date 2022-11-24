4c36e668d2a6b6ff5e533388fb476cd3e56e7932
Nathan Joudet <nathanjoudet@orange.fr>
2022-11-24 11:48:48 +0100

    Exercice1

---
+++ Exos.ts
@@ -0,0 +1,20 @@
+// Exercice 1
+// Créez une fonction qui prendra un prénom en paramètre. Votre fonction affichera :
+// "Bonjour " suivi du prénom dans un console.log.
+
+/**
+ * Type de paramètre => String
+ * Type retour de la fonction => void car elle ne retourne rien
+ * @returns String
+ */
+ function appel(prenom:string) :void  {
+    console.log("Bonjour " + prenom);
+}
+
+appel("Nathan");
+
+
+
+// Exercice 2
+// Créez une fonction qui prendra deux nombres en paramètre.
+// Votre fonction affichera la somme de ces deux nombres dans un console.log

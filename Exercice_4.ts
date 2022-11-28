7de6f784e497dd5d928f1d592846a57a488f0e3d
Nathan Joudet <nathanjoudet@orange.fr>
2022-11-28 09:23:36 +0100

    Exercice4 (Correction)

--- Exercice_4.ts
+++ Exercice_4.ts
@@ -1,3 +1,10 @@
+/**
+ * Fonction qui retourne le nombre le plus grand
+ * @param num1 number1
+ * @param num2 number2
+ * @param num3 number3
+ * @returns le nombre le plus grand
+ */
 // Exercice 4
 // Créer une fonction qui prendra 3 nombres en paramètre.
 // Votre fonction retournera le nombre de la plus grande valeur dans une phrase.
@@ -12,3 +19,16 @@ function lePlusGrand(num1: number, num2: number, num3: number): string {
   return `Le plus grand nombre est ${plusGrand}`;
 }
 console.log(lePlusGrand(15,78,6));
+
+
+// Exercice 4 (Correction)
+function plusGrandNombre(nombre1: number, nombre2: number, nombre3: number): string {
+  let plusGrandNombre: number = nombre1;
+  if (nombre2 > plusGrandNombre) {
+    plusGrandNombre = nombre2;
+  } else if (plusGrandNombre < nombre3) {
+    plusGrandNombre = nombre3;
+  }
+  return `Le plus grand nombre est ${plusGrandNombre}`;
+}
+console.log(plusGrandNombre(3,8,90));
